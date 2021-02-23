cfce50de4edbb09fffe88d89805e5851a3059d36
Juance11 <juancebastian99@gmail.com>
2021-02-22 20:26:19 -0500

    Pantalla reserva

---
+++ src/components/ReservaForm.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TablaHorarios from './TablaHorarios'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    input: {
+        display: 'none',
+    },
+}));
+
+
+
+const options = [
+    { title: 'Tienda 1' },
+    { title: 'Tienda 2' },
+    { title: 'Tienda 3' },
+    { title: 'Tienda 4' },
+    { title: 'Tienda 5' },
+    { title: 'Tienda 6' },
+    { title: 'Tienda 7' },
+    { title: 'Tienda 8' }
+]
+const servicios = [
+    { title: 'Peluqeria' },
+    { title: 'Paseo' }
+]
+
+export default function FormDialog(props) {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [tienda, setTienda] = React.useState('');
+    const [servicio, setServicio] = React.useState('');
+    const [fecha, setFecha] = React.useState(new Date());
+    const [horarios, setHorarios] = React.useState([]);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleSave = () => {
+        /*if (!tienda.length || !status.length || !name.length || !email.length || !dueDate) {
+            return;
+        }
+        var newItem = {
+            tienda: tienda,
+            servicio: servicio,
+            dueDate: dueDate,
+            responsible: {
+                name: name,
+                email: email
+            }
+        }
+        props.save(newItem);
+        setStatus("");
+        setTienda("");
+        setDueDate(new Date());
+        setName("");
+        setEmail("");
+        setOpen(false)*/
+    };
+
+    const handleTiendaChange = (e) => {
+        setTienda(e.target.value);
+    }
+    const handleServicioChange = (e) => {
+        setServicio(e.target.value);
+    }
+
+    const handleFechaChange = (e) => {
+        setFecha(e.target.value);
+    }
+
+    const buscarDisponibilidad = (e) => {
+        setHorarios([{
+            id: 1,
+            fecahini: "fecha ejemplo",
+            fechafin: "fecha ejemplo"
+        },
+        {
+            id: 2,
+            fechaini: "fecha ejemplo",
+            fechafin: "fecha ejemplo"
+        }, {
+            id: 3,
+            fechaini: "fecha ejemplo",
+            fechafin: "fecha ejemplo"
+        }
+        ]);
+    }
+
+    return (
+        <div className={classes.root}>
+            <Fab size="small" color="secondary" aria-label="add" onClick={handleClickOpen}>
+                <AddIcon />
+            </Fab>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title"
+                fullWidth={true}
+                maxWidth="xl"
+                scroll="paper">
+                <DialogTitle id="form-dialog-title">Crear reserva</DialogTitle>
+                <DialogContent>
+                    <Autocomplete
+                        id="tienda"
+                        options={options}
+                        onChange={handleTiendaChange}
+                        getOptionLabel={(option) => option.title}
+                        getOptionSelected={(option) => option.title}
+                        renderInput={(params) => (
+                            <TextField {...params} label="Tienda" margin="normal" />
+                        )}
+                    />
+                    <Autocomplete
+                        id="servicio"
+                        options={servicios}
+                        onChange={handleServicioChange}
+                        getOptionLabel={(option) => option.title}
+                        getOptionSelected={(option) => option.title}
+                        renderInput={(params) => (
+                            <TextField {...params} label="Servicio" margin="normal" />
+                        )}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="dueDate"
+                        type="date"
+                        onChange={handleFechaChange}
+                        fullWidth
+                    />
+                    <Button onClick={buscarDisponibilidad} variant="contained" color="primary">
+                        Buscar disponibilidad
+                    </Button>
+                    <TablaHorarios key="horarios" horarios={horarios} />
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
+}
+

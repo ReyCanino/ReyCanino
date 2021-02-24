38c1e5d8ccd8ff097ef4e8245f499a1472ea2f90
mariahv9 <mafehv1999@hotmail.com>
2021-02-24 13:57:04 -0500

    login finalizado

---
+++ src/components/LoginForm.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles} from '@material-ui/core/styles';
+import { Grid, Input, InputLabel, Link, Typography } from '@material-ui/core/';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import VpnKeySharpIcon from '@material-ui/icons/VpnKeySharp';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        overflow: 'hidden',
+        padding: theme.spacing(0, 3),
+    },
+    paper: {
+        maxWidth: 300,
+        margin: `${theme.spacing(2)}px auto`,
+        padding: theme.spacing(2),
+        height:'650px',
+        top:'50%',
+        left:'50%',
+        textAlign:'center',
+    },
+    button: {
+        backgroundColor:'#000',
+        color:'#FFF',
+        margin: '20px'
+    },
+    image: {
+        margin: '20px',
+        borderRadius:'20px',
+    },
+    form: {
+        margin: '20px',
+    }, 
+    letter: {
+        color:'grey',
+        size:'9px'
+    }
+}));
+
+export default function LoginForm() {
+const classes = useStyles();
+  
+return (
+    <div className={classes.root} >   
+        <Paper className={classes.paper}>
+            <Grid>
+                <h1>Bienvenido</h1>
+                <img src="/logo-gr.png" width='200' className={classes.image}/>
+                <FormControl className={classes.form} >
+                    <InputLabel htmlFor="user">Nombre de usuario</InputLabel>
+                        <Input
+                        id="user"
+                        startAdornment={
+                            <InputAdornment position="start">
+                            <AccountCircle/>
+                            </InputAdornment>
+                        }
+                        />
+                </FormControl>
+                <FormControl className={classes.form}>
+                    <InputLabel htmlFor="pwd" >Contraseña</InputLabel>
+                        <Input
+                        id="pwd"
+                        type="password"
+                        startAdornment={
+                            <InputAdornment position="start">
+                            <VpnKeySharpIcon/>
+                            </InputAdornment>
+                        }/>
+                </FormControl>
+                <div>
+                   <Button variant="contained" className={ classes.button } disableElevation>
+                    Ingresar
+                    </Button>  
+                </div>
+                <div>
+                    <Typography className={classes.letter}> 
+                        ¿Eres nuevo en Rey Canino?
+                    </Typography>
+                    <Link
+                    component="button"
+                    variant="body2"
+                    onClick={() => {
+                        console.info("Crear cuenta");
+                    }}
+                    > Crea tu cuenta
+                    </Link> 
+                </div> 
+            </Grid>
+        </Paper>
+    </div>
+);
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,25 @@
+import { makeStyles} from '@material-ui/core/styles';
 import React, { Component } from 'react';
+import LoginForm from '../components/LoginForm';
+
+const useStyles = makeStyles((theme) => ({
+
+}));
 
 export default class Login extends Component {
-
+    
     render() {
         return (
-            <h2>Login</h2>
+            <div 
+            style={{
+                backgroundImage: `url("../fondo-login.png")`,
+                backgroundRepeat: 'no-repeat',
+                width:'1250',
+                height:'730px',
+                color:'white'
+            }}>
+                <LoginForm/>
+            </div>
         );
     }
 }

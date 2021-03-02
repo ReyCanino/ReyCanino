fdb4c329e6a0bfac71c6f400d95b6927593ad930
GYEISSON <yeison7f@gmail.com>
2021-03-02 16:19:14 -0500

    feature tabla

--- src/components/MaganeReservations.js
+++ src/components/MaganeReservations.js
@@ -10,52 +10,50 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'name', label: 'Nombre', minWidth: 170 },
+  { id: 'date', label: 'Fecha', minWidth: 100 },
   {
-    id: 'population',
-    label: 'Population',
+    id: 'hour',
+    label: 'Hora',
     minWidth: 170,
     align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
+    id: 'pet',
+    label: 'Mascota',
     minWidth: 170,
     align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    
   },
   {
-    id: 'density',
-    label: 'Density',
+    id: 'service',
+    label: 'Servicio',
     minWidth: 170,
     align: 'right',
-    format: (value) => value.toFixed(2),
   },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData(name, date, hour, pet, service) {
+  return { name, date, hour, pet, service};
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
+  createData('Miguel Casteellanos', '02/03/2021', '12:30', 'Perro', 'Peluquería'),
 ];
 
 const useStyles = makeStyles({
@@ -65,6 +63,7 @@ const useStyles = makeStyles({
   container: {
     maxHeight: 440,
   },
+
 });
 
 export default function StickyHeadTable() {
@@ -85,13 +84,16 @@ export default function StickyHeadTable() {
     <Paper className={classes.root}>
       <TableContainer className={classes.container}>
         <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
+          <TableHead >
+            <TableRow >
               {columns.map((column) => (
                 <TableCell
                   key={column.id}
                   align={column.align}
-                  style={{ minWidth: column.minWidth }}
+                  style={{ 
+                      minWidth: column.minWidth,
+                      fontWeight: 'bold',
+                  }}
                 >
                   {column.label}
                 </TableCell>

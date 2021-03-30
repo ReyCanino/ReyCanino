927e9dccaca572a3c603a0ec6373f508255e4609
Juance11 <juancebastian99@gmail.com>
2021-03-30 17:59:33 -0500

    Build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Container, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({

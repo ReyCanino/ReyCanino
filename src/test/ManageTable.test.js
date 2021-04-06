e9dd62b3b96579b56b27f8fc2b02738e57e8b00e
mariahv9 <mafehv1999@hotmail.com>
2021-04-06 10:36:21 -0500

    Render

---
+++ src/test/ManageTable.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import ManageReservation from '../components/ManageReservations';
+
+test('renders learn react link', () => {
+  render(<ManageReservation />);
+});
